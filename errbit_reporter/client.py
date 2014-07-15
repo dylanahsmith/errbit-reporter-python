14c611cf2ab4ab20112168e17b247d377d533486
Dylan Thacker-Smith <Dylan.Smith@shopify.com>
2014-07-15 15:25:16 -0400

    Add a Client.notify_on_exception context manager to notify errbit.

--- errbit_reporter/client.py
+++ errbit_reporter/client.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+
 from six.moves import urllib
 
 from errbit_reporter import Notice, NoticeMetadata
@@ -7,6 +9,44 @@ class Client(object):
 
     def __init__(self, config):
         self.config = config
+
+    @contextmanager
+    def notify_on_exception(self, request_url=None, component=None, action=None,
+                            params={}, session={}, cgi_data={}, timeout=None):
+        """Context manager that notifies errbit of any exceptions.
+
+        The exception will be re-raised after notifying errbit.
+
+        Parameters
+        ----------
+        request_url : str, optional
+            The url field that will be shown by errbit on the error page
+        component : str, optional
+            The 1st part of the *where* field on the error page in errbit
+        action : str, optional
+            The 2nd part of the *where* field on the error page in errbit
+        params : dict of str, {str, list, dict} pairs, optional
+            The data shown in the Parameters tab with the error in errbit.
+        session : dict of str, {str, list, dict} pairs, optional
+            The data shown in the Session tab with the error in errbit.
+        cgi_data : dict of str, {str, list, dict} pairs, optional
+            The data shown in the Environment tab with the error in errbit.
+        timeout : int, optional
+            The timeout in seconds for the request to errbit (the default is no
+            timeout)
+        """
+        try:
+            yield
+        except Exception:
+            notice = Notice.from_exception(self.config)
+            notice.request_url = request_url
+            notice.component = component
+            notice.action = action
+            notice.params = params
+            notice.session = session
+            notice.cgi_data = cgi_data
+            self.send_notice(notice, timeout=timeout)
+            raise
 
     def notify(self, exc_info=None, request_url=None, component=None,
                action=None, params={}, session={}, cgi_data={}, timeout=None):
--- test/client_test.py
+++ test/client_test.py
@@ -1,6 +1,7 @@
 import os.path
 import sys
 import unittest
+import traceback
 
 import six
 from six.moves import urllib
@@ -40,26 +41,49 @@ urllib.request.install_opener(opener)
 
 
 class ClientTest(unittest.TestCase):
-    def tearDown(self):
-        TestHandler.request = None
-        TestHandler.response = None
-
-    def test_notify(self):
+    def setUp(self):
         response_filename = os.path.join(os.path.dirname(__file__), 'fixtures', 'response.xml')
         with open(response_filename, 'rb') as f:
             body = f.read()
         TestHandler.response = FakeResponse(body)
 
-        config = Configuration('apikey', 'http://localhost')
-        client = Client(config)
+        self.config = Configuration('apikey', 'http://localhost')
+        self.client = Client(self.config)
+
+    def tearDown(self):
+        TestHandler.request = None
+        TestHandler.response = None
+
+    def test_notify(self):
         try:
             int('a')
-        except:
-            expected_body = Notice.from_exception(config, sys.exc_info()).serialize()
-            metadata = client.notify()
+        except Exception:
+            expected_body = Notice.from_exception(self.config, sys.exc_info()).serialize()
+            metadata = self.client.notify()
         self.assertEqual(TestHandler.request.get_full_url(), "http://localhost/notifier_api/v2/notices/")
         self.assertEqual(TestHandler.request.get_method(), "POST")
         self.assertEqual(TestHandler.request.data, expected_body)
         self.assertEqual(metadata.id, '87186dda0c1d88569a171698')
         self.assertEqual(metadata.err_id, '7eafc85735047f2e5c9272b7')
         self.assertEqual(metadata.problem_id, '4886aba098109edbeb67a7f0')
+
+    def test_notify_on_exception(self):
+        inner_exc_info = None
+        try:
+            with self.client.notify_on_exception():
+                try:
+                    int('a')
+                except Exception:
+                    inner_exc_info = sys.exc_info()
+                    raise
+        except Exception:
+            self.assertEqual(sys.exc_info(), inner_exc_info)
+
+        self.assertEqual(TestHandler.request.get_full_url(), "http://localhost/notifier_api/v2/notices/")
+        self.assertEqual(TestHandler.request.get_method(), "POST")
+        filename, line_number, function_name, text = traceback.extract_tb(inner_exc_info[2])[0]
+        top_frame = '<line file="%s" method="%s" number="%d" />' % (filename, function_name, line_number)
+        self.assertIn(top_frame.encode('utf-8'), TestHandler.request.data)
+
+if __name__ == '__main__':
+    unittest.main()
